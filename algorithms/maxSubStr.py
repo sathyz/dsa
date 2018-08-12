8f9889e4b6ac04f2c07901292eeaff4eaf9ae547
Satheesh Kumar Mohan <sathyz@gmail.com>
2018-08-12 22:38:08 +0530

    yield substr only on hit.

--- algorithms/maxSubStr.py
+++ algorithms/maxSubStr.py
@@ -1,42 +1,47 @@
 """
-Given a stream and a set of characters. Get the max substring which contains all
-of the given characters.
+Given a stream and a set of characters. Get the max substring which contains
+all of the given characters.
 
 The characters can be in any order.
 """
-
 from collections import defaultdict
 
 
-def is_hit(count_given, given):
+def is_hit(counter, given):
     for ch in given:
-        if ch not in count_given:
+        if ch not in counter:
             return False
     return True
 
 
-def main(stream, given):
-    max_length = 0  # max length of matched substring found so far
+def preprocess(stream, given):
+    buffer = []
     count_given = defaultdict(int)
-    length = 0
-    start = -1  # start index of max substring found so far
-
-    for i, ch in enumerate(stream):  # O(S) => S size of stream.
+    for ch in stream:
         if ch in given:
+            buffer.append(ch)
             count_given[ch] += 1
-            length += 1
-        else:
+        elif buffer:
             if is_hit(count_given, given):
-                if length > max_length:
-                    max_length = length
-                    start = i - length
+                yield "".join(buffer)
+            buffer = []
             count_given = defaultdict(int)
-            length = 0
-    return stream[start:start + max_length]
+
+
+def main(stream, given):
+    max_length = 0
+    res = None
+    for substr in preprocess(stream, given):
+        length = len(substr)
+        if length > max_length:
+            max_length = length
+            res = substr
+    return res
 
 
 if __name__ == '__main__':
-    stream = "this is sample abcd text with xabcdas abcddy xabcdrdbcy abb cdxabcd"
+    stream = """this is sample abcd text with xabccas abcddy xabcdrdbcy abb
+cdxabcd. defcabddca zxdddacccb ghf bbbcddddkkkkl lijabcdfg."""
     given = ["a", "b", "c", "d"]
     substr = main(stream, given)
     print(substr)
