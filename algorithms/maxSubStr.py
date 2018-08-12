790b303c3cace5f4f556854eb8fd52e965ae1c79
Satheesh Kumar Mohan <sathyz@gmail.com>
2018-08-12 19:26:06 +0530

    Avoid size() using inline computation.

--- algorithms/maxSubStr.py
+++ algorithms/maxSubStr.py
@@ -1,9 +1,12 @@
 """
 Given a stream and a set of characters. Get the max substring which contains all
 of the given characters.
+
+The characters can be in any order.
 """
 
 from collections import defaultdict
+
 
 def is_hit(count_given, given):
     for ch in given:
@@ -11,35 +14,29 @@ def is_hit(count_given, given):
             return False
     return True
 
-def size(count_given):
-    length = 0
-    for ch, count in count_given.items():
-        length += count
-    return length
 
 def main(stream, given):
+    max_length = 0  # max length of matched substring found so far
     count_given = defaultdict(int)
+    length = 0
+    start = -1  # start index of max substring found so far
 
-    max_length = 0
-    start = -1
-
-    for i, ch in enumerate(stream):
+    for i, ch in enumerate(stream):  # O(S) => S size of stream.
         if ch in given:
             count_given[ch] += 1
+            length += 1
         else:
             if is_hit(count_given, given):
-                length = size(count_given)
                 if length > max_length:
                     max_length = length
-                    start = i - max_length
+                    start = i - length
             count_given = defaultdict(int)
-        print(start, max_length)
-    print(start, max_length)
+            length = 0
     return stream[start:start + max_length]
 
 
 if __name__ == '__main__':
-    stream = "this is sample text with xabc abcddy xabcddbcy abb cdxabcd"
+    stream = "this is sample abcd text with xabcdas abcddy xabcdrdbcy abb cdxabcd"
     given = ["a", "b", "c", "d"]
     substr = main(stream, given)
     print(substr)
